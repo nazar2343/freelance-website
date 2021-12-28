581faa5afdc1d86b662a688ccc6c0909b4cb6afd
nazar2343 <barannazar234@gmail.com>
2021-12-28 13:01:11 +0000

    React

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Test1 from './Test1';
 import Header from './Header/Header';
+import Goods from './Goods';
 
 
 const headerData = {
@@ -13,12 +14,18 @@ const headerData = {
   ]
 }
 
+const goods = [
+  {"title": "apple", "cost" : 300, "image":'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-256.png'},
+  {"title": "pear", "cost" : 500, "image":'https://cdn3.iconfinder.com/data/icons/fruits-52/150/icon_fruit_pera-256.png'},
+]
+
 
 function App() {
   return (
     <>
     <Header data = {headerData}></Header>
     <Test1></Test1>
+    {goods.map(item => <Goods title = {item.title} cost={item.cost} image={item.image} />)}
     </>
   );
 }
